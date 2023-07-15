0e7a3487a44745036253b96858fe600ccd980004
Helton Brito <heltoncarlospa@hotmail.com>
2023-07-15 16:15:45 -0300

    feat: create product category controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { CreateProductController } from "./controller/CreateProductController";
+import { CreateCategoryController } from "./controller/CreateCategoryController";
+import { CreateProductCategoryController } from "./controller/CreateProductCategoryController";
 
 const router = Router();
 
 const CreateProduct = new CreateProductController;
+const CreateCategory= new CreateCategoryController;
+const CreateProductCategory= new CreateProductCategoryController;
 
 router.post("/product", CreateProduct.handle);
+router.post("/category", CreateCategory.handle);
+router.post("/categoryProduct", CreateProductCategory.handle);
 
 export { router };
